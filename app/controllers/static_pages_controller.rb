b7180a5bffb3c92db92ea9bcca99793505fde8ee
brandon jones <brandon.jones9@gmail.com>
2015-09-12 22:55:18 -0700

    master commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+
+  def home
+    # Scraper.scrape_all
+    # @saved_searches = current_user ? current_user.saved_searches : []
+    # @all_job_searches = JobSearch.all
+  end
+
+end

6bab931a366ea9da996151983851e5451f8c97e1
brandon jones <brandon.jones9@gmail.com>
2015-09-25 11:08:46 -0700

    moved about my machines to redis too

--- app/models/about_my_machines.rb
+++ app/models/about_my_machines.rb
@@ -3,24 +3,12 @@ class AboutMyMachines
   attr_accessor :description
 
   def initialize
-    File.open(file_path, "r") do |f|
-      @description = f.read
-    end
-    return self
+      @description = $redis.get('about_my_machines')
   end
 
   def update(data)
-    File.open(file_path, "w+") do |f|
-      f.write(data[:description])
-    end
+    $redis.set('about_my_machines',data[:description])
     @description = data[:description]
     return self
-  end
-
-  private
-
-  def file_path
-    return "config/about_my_machines.txt" if Rails.env == 'development'
-    return ENV["APP_PATH"] + "/shared/config/about_my_machines.txt"
   end
 end

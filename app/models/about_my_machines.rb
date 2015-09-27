600a80f55e7a791a05d98efcad09ac168d9cf8e2
brandon jones <brandon.jones9@gmail.com>
2015-09-26 21:01:26 -0700

    more pretty and file upload
    added boxes around most of the screen and added abilty to upload new images for the about my machines section

--- app/models/about_my_machines.rb
+++ app/models/about_my_machines.rb
@@ -9,6 +9,26 @@ class AboutMyMachines
   def update(data)
     $redis.set('about_my_machines',data[:description])
     @description = data[:description]
+    if (data[:image].present?)
+      save_new_file data[:image]
+    end
     return self
+  end
+
+  def save_new_file(image)
+    extension = File.extname(image.tempfile)
+    new_file_name = 'mini_me'
+    # File.delete(Dir.pwd + '/app/assets/images/' + get_file) if get_file && File.file?(Dir.pwd + '/app/assets/images/' + get_file) 
+    FileUtils.cp(image.tempfile, Dir.pwd + '/app/assets/images')
+    File.rename(Dir.pwd+'/app/assets/images/'+File.basename(image.tempfile), Dir.pwd+'/app/assets/images/' + new_file_name + extension)
+    $redis.set(AboutMyMachines.redis_key, new_file_name + extension)
+  end
+
+  def get_file
+    return $redis.get(AboutMyMachines.redis_key)
+  end
+
+  def self.redis_key
+    return 'about_my_machines_image'
   end
 end
